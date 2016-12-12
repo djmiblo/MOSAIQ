e03447b44ca18810085eb5e3ca3381386c97956c
isutare412 <isutare412@gmail.com>
2016-12-13 02:23:42 +0900

    fixed clientModel.js

--- server/clientModel.js
+++ server/clientModel.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 request({
-  url: 'http://localhost:41212/news', //URL to hit
+  url: 'http://localhost:41212', //URL to hit
   qs: {date: '20161212'}, //Query string data
   method: 'GET', //Specify the method
   headers: { //We can define headers too
