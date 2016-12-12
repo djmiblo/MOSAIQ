2123b7ae4adfe2a073138438d91672a4c8ffe86f
isutare412 <isutare412@gmail.com>
2016-12-12 16:27:47 +0900

    plainText.js analyze text and count words

--- server/plainText.js
+++ server/plainText.js
@@ -1,5 +1,18 @@
 var async = require('async');
 var mysql = require('mysql');
+var $;
+require("jsdom").env("", function(err, window) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  $ = require("jquery")(window);
+});
+
+function decodeStr(str) {
+  return $('<div/>').html(str).text();
+}
 
 var client = mysql.createConnection({
   user: 'root',
@@ -7,16 +20,32 @@ var client = mysql.createConnection({
   database: 'MOSAIQ'
 })
 
-client.query('SELECT body FROM news WHERE news_id="1668"',
+client.query('SELECT body FROM news',
   function(err, rows) {
-    if
-      (err) console.log(err);
+    if (err)
+      console.log(err);
     else {
       var body = rows[0].body;
 
       var regexp = /\<[^\<\>]+\>/g;
       body = body.replace(regexp, '');
-      console.log(body);
+      body = decodeStr(body);
+
+      regexp = /[^\s]+/g;
+      var arr = body.match(regexp);
+
+      var counts = {};
+      arr.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+
+      var sortable = [];
+      for (var word in counts)
+        sortable.push([word, counts[word]])
+
+      sortable.sort(function(a, b) {
+          return b[1] - a[1]
+      })
+
+      console.log(sortable);
 
       process.exit();
     }
