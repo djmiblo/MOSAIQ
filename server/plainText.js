2735c004a00bfabfb7978c8938e524d8b696edb0
isutare412 <isutare412@gmail.com>
2016-12-14 20:55:31 +0900

    made plainText.js that serves plain text for machine learning

--- server/plainText.js
+++ server/plainText.js
@@ -1,6 +1,9 @@
-var async = require('async');
+var http = require('http');
+var express = require('express');
+var xml = require('xml');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var mysql = require('mysql');
-var $;
 require("jsdom").env("", function(err, window) {
   if (err) {
     console.error(err);
@@ -10,44 +13,71 @@ require("jsdom").env("", function(err, window) {
   $ = require("jquery")(window);
 });
 
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+if (process.argv.length < 3) {
+  console.log('Usage: node plainText.js <password>');
+  process.exit();
+}
+
 function decodeStr(str) {
+  var regexp = /<[^<>]+>/g;
+  str.replace(regexp, '');
   return $('<div/>').html(str).text();
 }
 
-var client = mysql.createConnection({
-  user: 'root',
-  password: 'ghkfkd',
-  database: 'MOSAIQ'
+var allNews = [];
+var DBpassword = process.argv[2];
+
+function calcLen(str) {
+  return parseInt(str.length / 8);
+}
+
+function selectNews(date, callback) {
+  var client = mysql.createConnection({
+    user: 'root',
+    password: DBpassword,
+    database: 'MOSAIQ'
+  })
+
+  client.query('SELECT type, headline, body FROM news WHERE date=?', [date],
+    function(err, rows) {
+      if (err)
+        console.log(err);
+      else {
+        allNews = [];
+        rows.forEach(function(value) {
+          value.body = decodeStr(body);
+          allNews.push(value);
+        })
+        callback();
+      }
+    })
+}
+
+app.get('/', function(req, res) {
+  var date = req.query.date;
+  selectNews(date, function() {
+    res.set({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Origin': '*'
+    })
+
+    if (allNews.length != 0)
+      res.json(allNews);
+    else {
+      res.json({
+        error: "no data for " + date
+      });
+    }
+  })
 })
 
-client.query('SELECT body FROM news',
-  function(err, rows) {
-    if (err)
-      console.log(err);
-    else {
-      var body = rows[50].body;
-
-      var regexp = /\<[^\<\>]+\>/g;
-      body = body.replace(regexp, '');
-      body = decodeStr(body);
-
-      regexp = /[^\s]+/g;
-      var arr = body.match(regexp);
-
-      var counts = {};
-      arr.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
-
-      var sortable = [];
-      for (var word in counts)
-        sortable.push([word, counts[word]])
-
-      sortable.sort(function(a, b) {
-          return b[1] - a[1]
-      })
-
-      console.log(sortable);
-
-      process.exit();
-    }
-  }
-)
+http.createServer(app).listen(41211, function() {
+  console.log('Server Running at localhost:41211');
+})
