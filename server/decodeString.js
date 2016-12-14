11177300cd0f5f38d9a010b8f26a1dcfcdfae69d
isutare412 <isutare412@gmail.com>
2016-12-14 20:56:01 +0900

    added code to decode encoded string

---
+++ server/decodeString.js
@@ -0,0 +1,57 @@
+var async = require('async');
+var mysql = require('mysql');
+var $;
+require("jsdom").env("", function(err, window) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  $ = require("jquery")(window);
+});
+
+function decodeStr(str) {
+  return $('<div/>').html(str).text();
+}
+
+var client = mysql.createConnection({
+  user: 'root',
+  password: DBpassword,
+  database: 'MOSAIQ'
+})
+
+client.query('SELECT body FROM news',
+  function(err, rows) {
+    if (err)
+      console.log(err);
+    else {
+      var body = rows[210].body;
+
+      var regexp = /\<[^\<\>]+\>/g;
+      body = body.replace(regexp, '');
+      body = decodeStr(body);
+
+      console.log(body);
+
+      /*
+      regexp = /[^\s]+/g;
+      var arr = body.match(regexp);
+
+      var counts = {};
+      arr.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+
+      var sortable = [];
+      for (var word in counts)
+        sortable.push([word, counts[word]])
+
+      sortable.sort(function(a, b) {
+          return b[1] - a[1]
+      })
+
+      console.log(sortable);
+      */
+
+      process.exit();
+    }
+  }
+)
